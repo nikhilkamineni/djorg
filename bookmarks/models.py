48ffdc6841761c91455392552b5750f406fbd8c8
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-05-23 14:26:27 -0400

    Added new fields to Bookmark model; Added template/route/view for /bookmarks endpoint

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -5,4 +5,6 @@ class Bookmark(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     url = models.URLField('URL', unique=True)
     name = models.CharField(max_length=200)
-    notes= models.TextField(blank=True)
+    notes = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Bookmark
 # Create your views here.
+
+def index(request):
+    bookmarks = Bookmark.objects.all()
+    context = {
+        'bookmarks': bookmarks
+    }
+
+    return render(request, 'bookmarks/index.html', context)
