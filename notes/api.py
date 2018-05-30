8eef6156c361a756493521823a3fd35b43176241
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-05-29 20:16:00 -0400

    Notes API now only showing notes for logged in user

--- notes/api.py
+++ notes/api.py
@@ -21,3 +21,10 @@ class NoteSerializer(serializers.HyperlinkedModelSerializer):
 class NoteViewSet(viewsets.ModelViewSet):
     serializer_class = NoteSerializer
     queryset = Note.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_anonymous:
+            return Note.objects.none()
+        else:
+            return Note.objects.filter(user=user)
